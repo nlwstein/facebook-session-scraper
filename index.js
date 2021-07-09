e61494ad6545c15a36d81f6913b8117a6d25fa72
Nicholas Stein <nick@nickstein.net>
2021-07-09 04:09:32 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer-extra')
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+const express = require('express');
+var bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+
+app.post('/api/login', function (req, res) {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({ validation: false, message: "Missing username or password.", body: req.body, data: req.data });
+  }
+  (async () => {
+    try {
+      puppeteer.use(StealthPlugin())
+      puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
+      const browser = await puppeteer.launch(); 
+      const page = await browser.newPage();
+      await page.goto("https://www.facebook.com/");
+      await page.waitForSelector("#email");
+      await page.type("#email", req.body.email);
+      await page.type("#pass", req.body.password);
+      await page.click('button[name="login"]');
+      await page.waitForSelector("#approvals_code");
+      await page.type("#approvals_code", req.body.twofactor);
+      await page.click("#checkpointSubmitButton");
+      await page.waitForSelector("div[role=feed]");
+      let cookies = await page.cookies();
+      cookies = cookies.map(({ name: key, ...rest }) => ({ key, ...rest }));
+      await browser.close();
+      return res.status(200).json({ appstate: cookies });
+    } catch (e) {
+      return res.status(500).json({ error: e });
+    }
+  })();
+});
+
+// Start server
+app.listen(process.env.PORT || 3000)
